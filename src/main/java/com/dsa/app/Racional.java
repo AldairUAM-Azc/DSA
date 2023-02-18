79621f53abdb90c11b558b255fbb0f90bc2113d9
AldairUAM-Azc <108377001+AldairUAM-Azc@users.noreply.github.com>
2023-02-18 03:43:55 +0000

    Racional

--- src/main/java/com/dsa/app/Racional.java
+++ src/main/java/com/dsa/app/Racional.java
@@ -3,25 +3,42 @@ package com.dsa.app;
 public class Racional {
     int num;
     int den;
+    
     public Racional(){
         this.num = 1;
         this.den = 1;
     }
+
     public  Racional(int num, int den){
-        this.num = num;
-        this.den = den;
+        int divisor = gcd(num, den);
+        this.num = num/divisor;
+        this.den = den/divisor;
     }
 
     public Racional suma(Racional a){
-        return new Racional(this.num*a.den + this.den*a.num, this.den*a.den);
+        return new Racional(this.num*a.den + this.den*a.num, this.den*a.den).simplify();
     }
     
     public static Racional resta(Racional a, Racional b){
-        return new Racional(a.num*b.den - a.den * b.num, a.den * b.den);
+        return new Racional(a.num*b.den - a.den * b.num, a.den * b.den).simplify();
     }
 
-    public String getString(){
-        String string = new String(this.num + "/" + this.den);
-        return string;
+    @Override
+    public String toString() {
+        return num + " / " + den;
+    }
+
+    private static int gcd(int p, int q) {
+        if (q == 0) {
+            return p;
+        }
+        return gcd(q, p % q);
+    }
+
+    private Racional simplify(){
+        int divisor = gcd(this.num, this.den);
+        this.num /= divisor;
+        this.den /= divisor;
+        return this;
     }
 }
