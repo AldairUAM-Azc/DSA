65e0e556a4dff43804b58c96cbd4d991d6b0d979
AldairUAM-Azc <108377001+AldairUAM-Azc@users.noreply.github.com>
2023-05-26 07:22:44 +0000

    ListaEstatica /Fila

--- src/main/java/com/dsa/filas/Fila.java
+++ src/main/java/com/dsa/filas/Fila.java
@@ -1,29 +1,89 @@
-package filas;
+package com.dsa.filas;
 
-import com.dsa.pilas.PilaDinamica;
+import java.util.Scanner;
 
-class Fila<T> {
-  // TODO: dequeue enqueue vacio y copia/clone 
-  // TODO: tarta, doble entrada a,b,c -> a,a,b,b,c,c
+class Fila {
+  private int[] info;
+  private int ultimo;
 
-  Fila<T>copia(){
-    return this.clone();
+  // elementos repetidos
+  // asi como llegan asi se ordenan
+  public Fila() {
+    ultimo = 0;
+    System.out.println("Tamanio maximo: ");
+    Scanner s = new Scanner(System.in);
+    int max = s.nextInt();
+    s.close();
+    info = new int[max];
   }
 
+  public Fila(int max) {
+    ultimo = 0;
+    info = new int[max];
+  }
 
-  void espejo(){
-    Fila<T>temp = this.copia();
-    PilaDinamica<T> pila = new PilaDinamica();
-    while (!this.vacio()){
-      T elem = this.dequeue();
-      temp.enqueue(elem);
-      pila.push(elem);
+  public boolean vacia() {
+    return ultimo == -1;
+  };
+
+  public int buscar(int x) {
+    int i = 0;
+    while (i <= ultimo
+        && x - info[i] < 0) { // This Integer casting is BS, Fix it asap
+      i++;
     }
-    while(!temp.vacio()){
-      this.enqueue(copia.dequeue());
-    }
-    while(!pila.vacia()){
-      this.enqueue(pila.pop());
+    return i;
+  }
+
+  public void insertar(int x) {
+    if (ultimo >= info.length - 1) {
+      System.out.println("Nos quedamos sin espacio mano");
+    } else {
+      int pos = buscar(x);
+      if (info[pos] == x) {
+        System.out.println("El elemento '" + x + "' ya existe");
+        return;
+      }
+      int aux = ++ultimo;
+      while (aux > pos) { // shift the elements passing the newly added element
+        info[aux] = info[aux - 1];
+        aux--;
+      }
+      info[pos] = x;
     }
   }
+
+  public void borrar(int x) {
+    int p = buscar(x);
+    if (!(info[p] == x)) {
+      System.out.println("Error: ele elemento no se encuentra");
+    } else {
+      for (int i = p; i <= ultimo - 2; i++) {
+        info[i] = info[i + 1];
+      }
+      ultimo--;
+    }
+  }
+
+  @Override
+  public String toString() {
+    String str = new String("[ ");
+    for (int i = 0; i < ultimo; i++) {
+      str += info[i] + " ";
+    }
+    return str + "]";
+  }
+
+  public static void main(String[] args) {
+    Fila miFila = new Fila(5);
+    miFila.insertar(2);
+    miFila.insertar(4);
+    miFila.insertar(6);
+    System.out.println(miFila);
+    miFila.borrar(6);
+    System.out.println(miFila);
+    miFila.borrar(2);
+    System.out.println(miFila);
+  }
+
 }
